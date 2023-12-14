a8787ad8fd227bbdb868157a30dc414b997f70d4
Raman Gautam <vaibhav.kumar458@gmail.com>
2023-12-15 02:53:18 +0530

    Added EJS layout

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+const expressLayouts = require("express-ejs-layouts");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./assets"));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // app.use("/", (req, res) => {
 //   return res.render("home", {
