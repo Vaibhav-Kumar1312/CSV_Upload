05ded6a4ffed471c6365b2e0689e379104db6090
Raman Gautam <vaibhav.kumar458@gmail.com>
2024-01-15 13:05:35 +0530

    sort functionality added

--- assets/js/csv_file.js
+++ assets/js/csv_file.js
@@ -1,9 +1,11 @@
 let initialTable = document.querySelector("#csv-data-table");
+let tableHeader = document.querySelector("#table-header");
 let originaltBody = document.querySelector(".original-table");
 let searchtBody = document.querySelector(".search-table");
 let searchInput = document.querySelector(".search-bar");
 let data = initialTable.dataset.csv;
 let csvArray = JSON.parse(data);
+let sortData = [];
 
 function displaySearchTable(objectArray) {
   if (objectArray.length <= 0) {
@@ -33,7 +35,7 @@ searchInput.addEventListener("keyup", function (e) {
   if (e.target.value !== "") {
     for (let i = 0; i < csvArray.length; i++) {
       let valueArray = Object.values(csvArray[i]);
-      // console.log(valueArray);
+      console.log(valueArray);
       // valueArray.forEach((item) => {
       //   if (item.toLowerCase().includes(e.target.value.toLowerCase())) {
       //     newData.push(csvArray[i]);
@@ -48,11 +50,49 @@ searchInput.addEventListener("keyup", function (e) {
       }
     }
   }
+  sortData = [...newData];
   displaySearchTable(newData);
-  console.log(newData);
+  // console.log(newData);
+  console.log(sortData);
 });
 // function renderTable(htmlElement) {
 // tableRow;
 // }
 // console.log(JSON.parse(JSON.stringify(jsonData)));
 // displaySearchTable(csvArray);
+tableHeader.addEventListener("click", function (e) {
+  console.log(e.target.innerHTML);
+  if (sortData.length) {
+    sortTableData(e.target.innerHTML, sortData);
+    return;
+  }
+  sortTableData(e.target.innerHTML, csvArray);
+});
+function sortTableData(key, tableData) {
+  const data = [...tableData];
+  // console.log(data[0][key]);
+  // console.log(Number.isNaN(Number(data[0][key])));
+  if (!Number.isNaN(Number(data[0][key]))) {
+    console.log("number sort");
+    data.sort((a, b) => a[key] - b[key]);
+  }
+  if (Number.isNaN(Number(data[0][key]))) {
+    console.log("string sort");
+    data.sort((a, b) => {
+      let fa = a[key].toLowerCase();
+      let fb = b[key].toLowerCase();
+      if (fa > fb) {
+        return 1;
+      }
+      if (fa < fb) {
+        return -1;
+      }
+    });
+  }
+  displaySearchTable(data);
+  // if (!isNaN(Date.parse(data[0][key]))) {
+  //   console.log("date sorted");
+  // }
+}
+
+// sortTableData(csvArray);
